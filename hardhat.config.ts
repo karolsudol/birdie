ea87e49a2d3b8acab91d601d760c1571c0da8792
elcomendante <karol@finsane.com>
2022-10-07 11:54:16 +0100

    deploy-prep

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "./tasks/ERC20-tasks.ts";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const SEPOLIA_URL = `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    // Add these lines -----------
+
+    // ---------------------------
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
+
+console.log(process.env.PRIVATE_KEY);
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,22 @@
 import { ethers } from "hardhat";
 
+const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const deployer = OWNER_ADDRESS;
+  console.log("Deploying contract with the account:", deployer);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const TokenERC20 = await ethers.getContractFactory("TravelSaver");
+  const erc20 = await TokenERC20.deploy(
+    "0xF631933E685441404012A558a95990dD6aB66096",
+    "0x2b5Fc7f001a173D49B29e34993bB2feF41Ccd803"
+  );
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await erc20.deployed();
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log("ERC20 deployed to:", erc20.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
