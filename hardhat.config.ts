64dcae40aa111dd32db1a1502e800ea450609be0
elcomendante <karol@finsane.com>
2022-10-07 13:09:30 +0100

    deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-import "./tasks/ERC20-tasks.ts";
+// import "./tasks/ERC20-tasks.ts";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const SEPOLIA_URL = `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`;
+const alfajores_URL = `https://alfajores-forno.celo-testnet.org`;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    sepolia: {
-      url: SEPOLIA_URL || "",
+    alfajores: {
+      url: alfajores_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+
+      // chainId: 44787,
     },
     // Add these lines -----------
 
     // ---------------------------
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: "USD",
+  // },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,22 @@
 import { ethers } from "hardhat";
 
 const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
+const USDC = "0xF631933E685441404012A558a95990dD6aB66096";
+const operatorWalletAddress = "0x2b5Fc7f001a173D49B29e34993bB2feF41Ccd803";
 
 async function main() {
   const deployer = OWNER_ADDRESS;
+  const usdc = USDC;
+  const fw = operatorWalletAddress;
+
   console.log("Deploying contract with the account:", deployer);
 
-  const TokenERC20 = await ethers.getContractFactory("TravelSaver");
-  const erc20 = await TokenERC20.deploy(
-    "0xF631933E685441404012A558a95990dD6aB66096",
-    "0x2b5Fc7f001a173D49B29e34993bB2feF41Ccd803"
-  );
+  const TravelSaver = await ethers.getContractFactory("TravelSaver");
+  const travelSaver = await TravelSaver.deploy(usdc, fw);
 
-  await erc20.deployed();
+  await travelSaver.deployed();
 
-  console.log("ERC20 deployed to:", erc20.address);
+  console.log("TravelSaver deployed to:", travelSaver.address);
 }
 
 main().catch((error) => {
